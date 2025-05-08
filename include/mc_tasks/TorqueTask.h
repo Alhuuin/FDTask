b8530f103a8b06148d1c3b505d2c06525288df8a
Alice CARIOU <alice.cariou@aist.go.jp>
2025-05-08 14:26:20 +0900

    initial commit

---
+++ include/mc_tasks/TorqueTask.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <mc_tasks/PostureTask.h>
+#include <RBDyn/FD.h>
+
+namespace mc_tasks
+{
+
+/**
+ * TorqueTask: Specify desired joint torques, injects the corresponding reference acceleration into a PostureTask backend.
+ */
+struct MC_TASKS_DLLAPI TorqueTask : public PostureTask
+{
+public:
+  TorqueTask(const mc_solver::QPSolver & solver, unsigned int rIndex, double weight = 1000.0);
+
+  //! Set the desired joint torques (size: nrDof)
+  void desiredTorque(const Eigen::VectorXd & tau);
+  //! Get the current desired joint torques
+  const Eigen::VectorXd & desiredTorque() const;
+
+   /*! \brief Get the current joint torques */
+  Eigen::VectorXd currentTorques() const;
+
+  void reset() override;
+  void update(mc_solver::QPSolver & solver) override;
+  void addToGUI(mc_rtc::gui::StateBuilder &) override;
+  void addToLogger(mc_rtc::Logger & logger) override;
+
+private:
+  Eigen::VectorXd desiredTorque_;
+  double dt_;
+  unsigned int rIndex_;
+  const mc_rbdyn::Robots & robots_;
+};
+
+} // namespace mc_tasks 
---
+++ src/mc_tasks/TorqueTask.cpp
@@ -0,0 +1,110 @@
+#include <mc_tasks/TorqueTask.h>
+#include <mc_tasks/MetaTaskLoader.h>
+#include <mc_rbdyn/Robot.h>
+#include <mc_solver/QPSolver.h>
+#include <mc_rtc/gui/NumberInput.h>
+#include <mc_rtc/log/Logger.h>
+#include <RBDyn/FD.h>
+
+namespace mc_tasks
+{
+
+TorqueTask::TorqueTask(const mc_solver::QPSolver & solver, unsigned int rIndex, double weight)
+: PostureTask(solver, rIndex, 0.1, weight),
+  dt_(solver.dt()),
+  rIndex_(rIndex),
+  robots_(solver.robots())
+{
+  desiredTorque_ = Eigen::VectorXd::Zero(robots_.robot(rIndex_).mb().nrDof());
+  type_ = "torque";
+  name_ = std::string("torque_") + robots_.robot(rIndex_).name();
+}
+
+void TorqueTask::desiredTorque(const Eigen::VectorXd & tau)
+{
+  desiredTorque_ = tau;
+}
+
+const Eigen::VectorXd & TorqueTask::desiredTorque() const
+{
+  return desiredTorque_;
+}
+
+Eigen::VectorXd TorqueTask::currentTorques() const
+{
+  const auto & jointTorque = robots_.robot(rIndex_).mbc().jointTorque;
+  const auto & mb = robots_.robot(rIndex_).mb();
+
+  Eigen::VectorXd result = Eigen::VectorXd::Zero(mb.nrDof());
+
+  for(size_t i = 0; i < jointTorque.size(); ++i)
+  {
+    const auto & joint = mb.joint(i);
+    const auto & joint_torques = jointTorque[i];
+
+    size_t dofIndex = mb.jointPosInDof(i);
+
+    for(size_t j = 0; j < joint.dof(); ++j)
+    {
+      if (j < joint_torques.size()) {
+        result(dofIndex + j) = joint_torques[j];
+      } else {
+        result(dofIndex + j) = 0.0;
+      }
+    }
+  }
+  return result;
+}
+
+void TorqueTask::reset()
+{
+  desiredTorque_.setZero();
+  PostureTask::reset();
+}
+
+void TorqueTask::update(mc_solver::QPSolver & solver)
+{
+  auto & robot = robots_.robot(rIndex_);
+  rbd::ForwardDynamics fd(robot.mb());
+  fd.computeH(robot.mb(), robot.mbc());
+  fd.computeC(robot.mb(), robot.mbc());
+  const Eigen::MatrixXd & M = fd.H();
+  const Eigen::VectorXd & Cg = fd.C();
+  Eigen::VectorXd refAccel = M.ldlt().solve(desiredTorque_ - Cg);
+  PostureTask::refAccel(refAccel);
+  PostureTask::update(solver);
+}
+
+void TorqueTask::addToLogger(mc_rtc::Logger & logger)
+{
+  logger.addLogEntry(name_ + "_desiredTorque", this, [this]() { return desiredTorque_; });
+  logger.addLogEntry(name_ + "_currentTorques", this, [this]() { return currentTorques(); });
+}
+
+void TorqueTask::addToGUI(mc_rtc::gui::StateBuilder & gui)
+{
+  MetaTask::addToGUI(gui);
+  gui.addElement({"Tasks", name_, "Torque"},
+                 mc_rtc::gui::NumberInput("Desired Torque", [this]() { return this->desiredTorque_; },
+                                           [this](const Eigen::VectorXd & tau) { this->desiredTorque(tau); }));
+}
+
+} // namespace mc_tasks
+
+namespace
+{
+static auto registered = mc_tasks::MetaTaskLoader::register_load_function(
+    "torque",
+    [](mc_solver::QPSolver & solver, const mc_rtc::Configuration & config)
+    {
+      const auto robotIndex = robotIndexFromConfig(config, solver.robots(), "torque");
+      auto t = std::make_shared<mc_tasks::TorqueTask>(solver, robotIndex, config("weight", 10.));
+      // Optionally load desired torque from config
+      if(config.has("desiredTorque"))
+      {
+        t->desiredTorque(config("desiredTorque"));
+      }
+      t->load(solver, config);
+      return t;
+    });
+} 
